ff5dc803cdfd71696a258aa6483ee73e03de04c2
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-09-09 14:58:28 -0300

    Removing comment

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Variáveis
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 const btnTry = document.querySelector("#btnTry")
@@ -6,22 +5,14 @@ const btnReset = document.querySelector("#btnReset")
 const randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
-// Eventos
 btnTry.addEventListener('click', handleTryClick)
 // addEventListener recebe 2 argumentos, uma string com o nome do evento e uma função callback que irá executar um código em reação ao evento
 btnReset.addEventListener('click', handleResetClick)
 document.addEventListener('keydown', handleEnterKey)
 
-/* 
-Funções callback:
-- Função passada como argumento de outra função
-- Função registrada para ser executada sempre que clicar
-- Função que não é executada automaticamente ao ler na aplicação
-*/
-
 function handleTryClick(event) {
     // a função handleTryClick recebe o evento event, um objeto com todos os dados do evento de clique
-    event.preventDefault() /* evitando o envio do formulário e o recarregamento automático da página ao clicar no botão */
+    event.preventDefault() // evitando o envio do formulário e o recarregamento automático da página ao clicar no botão
 
     const inputNumber = document.querySelector("#inputNumber")
 
